f3ba4989cad4c9714d0e0273aaf7ea4084f4094a
Patricia Ho <hopatricia1711@gmail.com>
2022-04-28 18:06:12 +0700

    update the posts button

--- resources/views/dashboard-admin/posts/input-article/create-article.blade.php
+++ resources/views/dashboard-admin/posts/input-article/create-article.blade.php
@@ -29,94 +29,53 @@
                                 <input type="text" class="form-control" id="inputAuthorArtikel" name="author">
                             </div>
                         </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="container-fluid pt-4 px-4">
-        <div class="row g-4">
-            <div class="col-sm-12">
-                <div class="bg-light rounded h-100 p-4 d-flex justify-content-between align-items-center">
-                    <h6 class="mb-4" style="margin-top:15px;">Input Foto untuk Artikel</h6>
-                    <button type="submit" class="btn btn-outline-success m-2">Tambah dokumentasi</button>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <br>
 
-    <div class="container-fluid pt-4 px-4">
-        <div class="row g-4">
-            <div class="col-sm-12">
-                <div class="bg-light rounded h-100 p-4">
-                    <div class="d-flex justify-content-between align-items-center" style="margin-bottom: 20px;">
-                        <h6 class="mb-4" style="margin-top:15px">Input Foto 1</h6>
-                    </div>
-                        <div class="row mb-3">
-                            <label for="inputDokumentasiDariEvent" class="col-sm-2 col-form-label">Event</label>
-                            <div class="col-sm-10" style="margin-bottom: 20px;">
-                                <select class="form-select form-control" id="inputDokumentasiDariEvent" name="id_event[]">
-                                    <option selected>Pilih...</option>
-                                @forelse ($event as $e)
-                                    <option value="{{ $e->id_dokumentasi }}">
-                                        <span class="idEvent">{{ $e->id_event }}</span> - <span class="namaEvent">{{ $e->nama_event }}</span>
-                                    </option>
-                                @empty
-                                    
-                                @endforelse
-                                @error('id_event')
-                                    {{ $message }}
-                                @enderror
-                                </select>
-                            </div>
+                        <div class="d-flex justify-content-between">
+                            <h6 class="mb-4" style="margin-top:15px;">Input Foto untuk Artikel</h6>
+                            <button type="button" id="add-documentation-event"
+                                class="add btn btn-outline-success m-2 add_field">Tambah dokumentasi foto</button>
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 
+                        <br>
 
-    <div class="container-fluid pt-4 px-4">
-        <div class="row g-4">
-            <div class="col-sm-12">
-                <div class="bg-light rounded h-100 p-4">
-                    <div class="d-flex justify-content-between align-items-center" style="margin-bottom: 20px;">
-                        <h6 class="mb-4" style="margin-top:15px">Input Foto 2</h6>
-                        <button type="submit" class="btn btn-outline-danger">X</button>
-                    </div>
-                        <div class="row mb-3">
+                        <div class="row mb-3" id="input-multiple-foto">
                             <label for="inputDokumentasiDariEvent" class="col-sm-2 col-form-label">Event</label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-9" style="margin-bottom: 20px;">
                                 <select class="form-select form-control" id="inputDokumentasiDariEvent" name="id_event[]">
                                     <option selected>Pilih...</option>
                                     @forelse ($event as $e)
-                                    <option value="{{ $e->id_dokumentasi }}">
-                                        <span class="idEvent">{{ $e->id_event }}</span> - <span class="namaEvent">{{ $e->nama_event }}</span>
-                                    </option>
-                                @empty
-                                    
-                                @endforelse
+                                        <option value="{{ $e->id_dokumentasi }}">
+                                            <span class="idEvent">{{ $e->id_event }}</span> - <span
+                                                class="namaEvent">{{ $e->nama_event }}</span>
+                                        </option>
+                                    @empty
+                                    @endforelse
+
+                                    @error('id_event')
+                                        {{ $message }}
+                                    @enderror
                                 </select>
-                                @error('id_event')
-                                    {{ $message }}
-                                @enderror
                             </div>
                         </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid pt-4 px-4">
-        <div class="row g-4">
-            <div class="col-sm-12">
-                <div class="bg-light rounded h-100 p-4">
-                        <button type="submit" class="btn btn-outline-warning m-2 float-end w-100">Submit Artikel</button>
+                        <button type="submit" class="btn btn-outline-warning m-2 float-end w-100">Submit Dokumentasi</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
     <!-- Form Article Ends -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script>
+        $(document).on('click', '.add_field', function() {
+            $('<div class="row mb-3" id="temporary"> <label for="inputDokumentasiDariEvent" class="col col-form-label">Event</label> <div class="col-sm-9" style="margin-bottom: 20px;"> <select class="form-select form-control" id="inputDokumentasiDariEvent" name="id_event[]"><option selected>Pilih...</option> @forelse ($event as $e) <option value="{{ $e->id_dokumentasi }}"> <span class="idEvent">{{ $e->id_event }}</span> - <span class="namaEvent">{{ $e->nama_event }}</span></option>@empty @endforelse @error('id_event'){{ $message }}@enderror</select></div> <div class="col-sm-1 d-flex justify-content-center align-items-start"> <input type="button" value="X" class="btn btn-outline-danger" id="remove-spesific-photo"></input> </div> </div>').insertAfter('#input-multiple-foto:last');
+        })
+
+        $(document).on('click', '#remove-spesific-photo', function() {
+            $('#temporary').remove();
+        })
+    </script>
 @endsection
