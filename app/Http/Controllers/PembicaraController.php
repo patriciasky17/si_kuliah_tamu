7bd6c93fbc4f39d88130c2f3cb73abf9838b480d
Patricia Ho <hopatricia1711@gmail.com>
2022-05-01 00:24:11 +0700

    detail event and pembicara controller

---
+++ app/Http/Controllers/PembicaraController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pembicara;
+use Illuminate\Http\Request;
+
+class PembicaraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pembicara = Pembicara::all();
+        return view('dashboard-admin.pembicara.detail-pembicara.detail-pembicara',[
+            'title' => 'Data Pembicara - Pradita University\'s Guest Lecturers',
+            'pembicara' => $pembicara
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard-admin.pembicara.input-pembicara.input-pembicara',[
+            'title' => 'Input Pembicara - Pradita University\'s Guest Lecturers',
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama' => 'required|string',
+            'institusi' => 'required|string',
+            'jabatan' => 'required|string',
+            'foto' => 'required|file|mimes:jpeg,png,jpg|max:3072',
+            'cv' => 'required|file|mimes:pdf,docx,doc|max:3072',
+            'npwp' => 'nullable|numeric',
+            'no_rekening' => 'nullable|numeric',
+            'sertifikat' => 'nullable|file|mimes:jpeg,png,jpg,pdf|max:3072',
+            'bank' => 'nullable|string',
+        ]);
+
+        $pembicara = [
+            'nama' => $validatedData['nama'],
+            'institusi' => $validatedData['institusi'],
+            'jabatan' => $validatedData['jabatan'],
+            'foto' => $validatedData['foto'],
+            'cv' => $validatedData['cv'],
+            'npwp' => $validatedData['npwp'],
+            'no_rekening' => $validatedData['no_rekening'],
+            'sertifikat' => $validatedData['sertifikat'],
+            'bank' => $validatedData['bank']
+        ];
+
+        return redirect()->intended(route('pembicara.index'))->with('success', 'Pembicara has been successfully added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/dashboard-admin/event/event/detail-event/detail-event.blade.php
+++ resources/views/dashboard-admin/event/event/detail-event/detail-event.blade.php
@@ -27,15 +27,15 @@
                 <h6 class="mb-0">Data Event Kuliah Tamu</h6>
             </div>
             <div class="table-responsive">
-                <table border="0" cellspacing="5" cellpadding="5">
-                    <tbody class="d-flex">
+                <table border="0" cellspacing="5" cellpadding="5" style="margin-bottom: 20px;">
+                    <tbody class="d-flex justify-content-center">
                         <tr>
                             <td>Minimum date:</td>
-                            <td><input type="text" id="min" name="min"></td>
+                            <td><input type="text" id="min" name="min" class="form-control"></td>
                         </tr>
                         <tr>
                             <td>Maximum date:</td>
-                            <td><input type="text" id="max" name="max"></td>
+                            <td><input type="text" id="max" name="max" class="form-control"></td>
                         </tr>
                     </tbody>
                 </table>
@@ -43,18 +43,14 @@
                 <table id="example" class="display" cellspacing="0" width="100%">
                     <thead>
                         <tr>
+                            <th scope="col">Num.</th>
                             <th scope="col">ID Event</th>
                             <th scope="col">Nama Event</th>
                             <th scope="col">Cara Pelaksanaan</th>
-                            <th scope="col">Background</th>
-                            <th scope="col">Flyer</th>
-                            <th scope="col">Tempat Pelaksanaan</th>
-                            <th scope="col">Link</th>
                             <th scope="col">Tanggal Pelaksanaan</th>
                             <th scope="col">Jam Mulai</th>
                             <th scope="col">Durasi</th>
                             <th scope="col">PIC</th>
-                            <th scope="col">Proposal</th>
                             <th scope="col">Pembicara</th>
                             <th scope="col">Details</th>
                         </tr>
@@ -62,18 +58,14 @@
 
                     <tfoot>
                         <tr>
+                            <th scope="col">Num.</th>
                             <th scope="col">ID Event</th>
                             <th scope="col">Nama Event</th>
                             <th scope="col">Cara Pelaksanaan</th>
-                            <th scope="col">Background</th>
-                            <th scope="col">Flyer</th>
-                            <th scope="col">Tempat Pelaksanaan</th>
-                            <th scope="col">Link</th>
                             <th scope="col">Tanggal Pelaksanaan</th>
                             <th scope="col">Jam Mulai</th>
                             <th scope="col">Durasi</th>
                             <th scope="col">PIC</th>
-                            <th scope="col">Proposal</th>
                             <th scope="col">Pembicara</th>
                             <th scope="col">Details</th>
                         </tr>
@@ -82,105 +74,137 @@
                     <tbody>
                         <tr>
                             <td>1</td>
+                            <td>1</td>
                             <td>Sesi 9 : Define - Design - Develop, Strategi Penerapan Transformasi Digital</td>
                             <td>Online</td>
-                            <td><a href="#">bg_transformasi_digital.jpg</a></td>
-                            <td><a href="#">flyer_transformasi_digital.jpg</a></td>
-                            <td>Webex</td>
-                            <td>https://ykgw.webex.com/ykgw/j.php?MTID=mee0a20ece781af062018afc0c9e0cd25</td>
                             <td>2022-02-23</td>
                             <td>09:00 WIB</td>
                             <td>1 Jam 30 Menit</td>
                             <td>Aditya</td>
-                            <td><a href="#">proposal_hpc_1.pdf</a></td>
                             <td></td>
-                            <<td class="d-flex align-items-center">
+                            <td class="d-flex align-items-center">
+                                <a class="btn btn-sm btn-outline-info w-100" href="#">Show</a>
                                 <a class="btn btn-sm btn-outline-warning w-100" href="#">Edit</a>
                                 <a class="btn btn-sm btn-outline-danger w-100" href="#">Delete</a>
                             </td>
                         </tr>
                         <tr>
                             <td>2</td>
+                            <td>2</td>
                             <td>Social Engineering dengan Dana</td>
                             <td>Online</td>
-                            <td><a href="#">bg_sc_dana.jpg</a></td>
-                            <td><a href="#">flyer_sc_dana.jpg</a></td>
-                            <td>Google Meet</td>
-                            <td>https://meet.google.com/gms-kftj-bqu</td>
                             <td>2022-03-25</td>
                             <td>09:00 WIB</td>
                             <td>1 Jam 30 Menit</td>
                             <td>Candra</td>
-                            <td><a href="#">proposal_se_1.pdf</a></td>
                             <td></td>
                             <td class="d-flex align-items-center">
+                                <a class="btn btn-sm btn-outline-info w-100" href="#">Show</a>
                                 <a class="btn btn-sm btn-outline-warning w-100" href="#">Edit</a>
                                 <a class="btn btn-sm btn-outline-danger w-100" href="#">Delete</a>
                             </td>
                         </tr>
                         <tr>
                             <td>3</td>
+                            <td>3</td>
                             <td>Pengembangan Software dengan Metode Scrum bersama Metrodata</td>
                             <td>Online</td>
-                            <td><a href="#">bg_software_metrodata.jpg</a></td>
-                            <td><a href="#">flyer_software_metrodata.jpg</a></td>
-                            <td>Google Meet</td>
-                            <td>https://meet.google.com/ujs-pqfz-kyk</td>
                             <td>2022-03-11</td>
                             <td>13:30 WIB</td>
                             <td>1 Jam 30 Menit</td>
                             <td>Arief</td>
-                            <td><a href="#">proposal_imk_1.pdf</a></td>
                             <td></td>
                             <td class="d-flex align-items-center">
+                                <a class="btn btn-sm btn-outline-info w-100" href="#">Show</a>
                                 <a class="btn btn-sm btn-outline-warning w-100" href="#">Edit</a>
                                 <a class="btn btn-sm btn-outline-danger w-100" href="#">Delete</a>
                             </td>
                         </tr>
                         <tr>
                             <td>4</td>
+                            <td>4</td>
                             <td>Effective Scheduling Program with C#</td>
                             <td>Online</td>
-                            <td><a href="#">bg_scheduling.jpg</a></td>
-                            <td><a href="#">flyer_scheduling.jpg</a></td>
-                            <td>Google Meet</td>
-                            <td>https://meet.google.com/vou-wwoa-hcb</td>
                             <td>2022-03-29</td>
                             <td>10:00 WIB</td>
                             <td>1 Jam 30 Menit</td>
                             <td>David</td>
-                            <td><a href="#">proposal_arsikom_1.pdf</a></td>
                             <td></td>
                             <td class="d-flex align-items-center">
+                                <a class="btn btn-sm btn-outline-info w-100" href="#">Show</a>
                                 <a class="btn btn-sm btn-outline-warning w-100" href="#">Edit</a>
                                 <a class="btn btn-sm btn-outline-danger w-100" href="#">Delete</a>
                             </td>
                         </tr>
                         <tr>
                             <td>5</td>
+                            <td>5</td>
                             <td>Blockchain: Immutability, Security, Transparency</td>
                             <td>Online</td>
-                            <td><a href="#">bg_transformasi_digital.jpg</a></td>
-                            <td><a href="#">flyer_transformasi_digital.jpg</a></td>
-                            <td>Zoom</td>
-                            <td>https://zoom.us/j/96968536381?pwd=MitNL0tiR05DcUE2OElCaU9SamJ6Zz09</td>
                             <td>2022-04-01</td>
                             <td>13:30 WIB</td>
                             <td>2 Jam 0 Menit</td>
                             <td>Putri</td>
-                            <td><a href="#">proposal_ml_1.pdf</a></td>
                             <td></td>
                             <td class="d-flex align-items-center">
+                                <a class="btn btn-sm btn-outline-info w-100" href="#">Show</a>
                                 <a class="btn btn-sm btn-outline-warning w-100" href="#">Edit</a>
                                 <a class="btn btn-sm btn-outline-danger w-100" href="#">Delete</a>
                             </td>
                         </tr>
                     </tbody>
                 </table>
+
             </div>
         </div>
     </div>
     <!-- Event End -->
+
+    <!-- Data Detail Event Starts Here -->
+    <div class="container-fluid pt-4 px-4">
+        <div class="bg-light text-justify rounded p-4">
+            <div class="d-flex align-items-center justify-content-between mb-4">
+                <h6 class="mb-0">Data Event - <span class="nama-event">Sesi 9 : Define - Design - Develop, Strategi Penerapan Transformasi Digital</span></h6>
+            </div>
+
+            <div class="row">
+                <div class="col-12" style="margin-bottom:30px">
+                    <dl class="row mb-0 d-flex">
+                        <dt class="col-sm-4">Cara Pelaksanaan</dt>
+                        <dd class="col-sm-8">Online</dd>
+
+                        <dt class="col-sm-4">Tempat/Media Pelaksanaan</dt>
+                        <dd class="col-sm-8">Webex</dd>
+
+                        <dt class="col-sm-4">Link</dt>
+                        <dd class="col-sm-8">https://ykgw.webex.com/ykgw/j.php?MTID=mee0a20ece781af062018afc0c9e0cd25</dd>
+
+                        <dt class="col-sm-4">Estimasi Waktu</dt>
+                        <dd class="col-sm-8"><span class="jam_mulai">09:00</span> - <span class="jam_selesai">10:30</span> WIB</dd>
+
+                        <dt class="col-sm-4">Pembicara</dt>
+                        <dd class="col-sm-8">-</dd>
+
+                        <dt class="col-sm-4">Laporan Akhir</dt>
+                        <dd class="col-sm-8"><a class="btn btn-sm btn-outline-info" href="#">Show Laporan Akhir</a></dd>
+                    </dl>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6" style="margin-top:30px">
+                    <h6 class="text-center" style="margin-bottom: 20px;">Background</h6>
+                    <div class="group-contains d-flex justify-content-center">
+                        <img class="img-fluid  mx-auto  bg-flyer-photo" src="./assets/img/profile-picture-test1.jfif">
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6" style="margin-top:30px">
+                    <h6 class="text-center" style="margin-bottom: 20px;">Flyer</h6>
+                    <div class="group-contains d-flex justify-content-center">
+                        <img class="img-fluid  mx-auto  bg-flyer-photo" src="./assets/img/profile-picture-test1.jfif">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Data Detail Event Ends Here -->
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
