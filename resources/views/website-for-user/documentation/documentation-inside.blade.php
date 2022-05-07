f8f948b11761bf0b3ccb4fa0adcaf4883fceecf5
Hael12122002 <hendralijaya@gmail.com>
2022-05-07 16:20:41 +0700

    a

--- resources/views/website-for-user/documentation/documentation-inside.blade.php
+++ resources/views/website-for-user/documentation/documentation-inside.blade.php
@@ -3,7 +3,7 @@
     <!-- Pop Up -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css">
 
-    <link rel="stylesheet" href="./assets/css/style-documentation-inside.css">
+    <link rel="stylesheet" href="/assets-user/css/style-documentation-inside.css">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
@@ -56,7 +56,7 @@
 
             <div class="col-md-12 col-sm-12">
                 <form class="button-download">
-                    <a href="./assets/pradita.mp4" download class="btn btn-outline-warning" role="button" aria-pressed="true">Download Video Dokumentasi</a>
+                    <a href="/assets-user/pradita.mp4" download class="btn btn-outline-warning" role="button" aria-pressed="true">Download Video Dokumentasi</a>
                 </form>
             </div>
 
@@ -72,8 +72,8 @@
 @endsection
 
 @section('js')
-    <script src ="./assets/js/script-magnetic-popup.js"></script>
-    <script src ="./assets/js/script.js"></script>
+    <script src ="/assets-user/js/script-magnetic-popup.js"></script>
+    <script src ="/assets-user/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 @endsection
